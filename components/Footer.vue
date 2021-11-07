1a254bd07563eeb3626d079024218becc71501fa
adnan aziz <deuteron6@gmail.com>
2021-11-07 12:20:36 +0700

    add template Header & Footer

---
+++ components/Footer.vue
@@ -0,0 +1,135 @@
+<template>
+  <v-container class="pa-0 footer" fluid>
+    <v-footer class="footer" color="#011B4B">
+      <section class="footer__container">
+        <v-row
+          style="width: 100%"
+          justify="space-around"
+          no-gutters
+          class="footer__row"
+        >
+          <v-col cols="12" sm="12" md="2" lg="2" xl="2" style="width: 100%">
+            <div class="footer__content ml-4 white--text">
+              <h4 class="footer__title">Higlob</h4>
+              <div class="footer__item text-left">
+                <span
+                  class="text-left text-capitalize"
+                  style="width: 30%; line-height: 183%"
+                  >Jalan turi km 3,7 perum pondok idaman no A2 kadisobo, <br />
+                  Kadisobo I / Pepen, Trimulyo, Kec. Sleman, <br />
+                  Kabupaten Sleman, Daerah Istimewa Yogyakarta 55513</span
+                >
+              </div>
+            </div>
+          </v-col>
+          <v-col cols="12" sm="12" md="3" lg="3" xl="3" style="width: 100%"
+            ><div class="footer__content white--text">
+              <h4 class="footer__title ml-5">Layanan</h4>
+              <div
+                class="
+                  footer__item
+                  d-flex
+                  justify-start
+                  align-start
+                  flex-wrap flex-column
+                "
+              >
+                <v-btn color="white" class="text-capitalize" text rounded>
+                  Persiapan Bahasa
+                </v-btn>
+                <v-btn color="white" class="text-capitalize" text rounded>
+                  Pendidikan Luar Negeri
+                </v-btn>
+                <v-btn color="white" class="text-capitalize" text rounded>
+                  Peluang Luar Negeri
+                </v-btn>
+                <v-btn color="white" class="text-capitalize" text rounded>
+                  Working Holiday
+                </v-btn>
+              </div>
+            </div></v-col
+          >
+          <v-col cols="12" sm="12" md="3" lg="3" xl="3" style="width: 100%">
+            <div class="footer__content white--text">
+              <h4 class="footer__title ml-5">Bantuan</h4>
+              <div
+                class="
+                  footer__item
+                  d-flex
+                  justify-start
+                  align-start
+                  flex-wrap flex-column
+                "
+              >
+                <v-btn color="white" class="text-capitalize" text rounded>
+                  Kontak Kami
+                </v-btn>
+                <v-btn color="white" class="text-capitalize" text rounded>
+                  FAQs
+                </v-btn>
+                <h4 class="footer__title ml-5">Ikuti Kami :</h4>
+                <div class="d-flex justify-center flex-wrap align-center">
+                  <v-btn
+                    v-for="icon in icons"
+                    :key="icon"
+                    class="mx-4 white--text"
+                    icon
+                  >
+                    <v-icon size="24px">
+                      {{ icon }}
+                    </v-icon>
+                  </v-btn>
+                </div>
+              </div>
+            </div>
+          </v-col>
+          <v-col cols="12" sm="12" md="2" lg="2" xl="2" style="width: 100%">
+            <div class="footer__content white--text">
+              <h4 class="footer__title ml-5">Perusahaan</h4>
+              <div
+                class="
+                  footer__item
+                  d-flex
+                  justify-start
+                  align-start
+                  flex-wrap flex-column
+                "
+              >
+                <v-btn color="white" class="text-capitalize" text rounded>
+                  Tentang Kami
+                </v-btn>
+                <v-btn color="white" class="text-capitalize" text rounded>
+                  Galeri
+                </v-btn>
+                <v-btn color="white" class="text-capitalize" text rounded>
+                  Penghargaan
+                </v-btn>
+              </div>
+            </div>
+          </v-col>
+          <v-col
+            align="center"
+            class="footer__copyright white--text mt-10"
+            cols="12"
+          >
+            Copyright © {{ new Date().getFullYear() }} PT Hi Global. All rights
+            reserved
+          </v-col>
+        </v-row>
+      </section>
+    </v-footer>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      icons: ['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram'],
+    }
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,201 @@
+<template>
+  <div>
+    <v-app-bar
+      absolute
+      color="white"
+      elevate-on-scroll
+      scroll-target="#scrolling-techniques-7"
+    >
+      <div
+        style="width: 100%"
+        :class="$vuetify.breakpoint.mdAndDown ? 'd-flex' : 'd-none'"
+      >
+        <v-toolbar-title class="mx-5">
+          <img
+            style="width: 40px"
+            :src="require('@/assets/images/logo.png')"
+            alt="logo higlob"
+          />
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-app-bar-nav-icon class="mx-5" @click="drawer = true"
+          ><img :src="require('@/assets/images/bar-chart.png')" alt=""
+        /></v-app-bar-nav-icon>
+      </div>
+
+      <v-container class="hidden-md-and-down">
+        <div class="d-flex align-center">
+          <v-toolbar-title>
+            <img
+              style="width: 40px"
+              :src="require('@/assets/images/logo.png')"
+              alt="logo higlob"
+            />
+          </v-toolbar-title>
+
+          <v-spacer></v-spacer>
+          <nuxt-link
+            class="mx-3"
+            style="text-decoration: none; color: black"
+            v-for="(item, index) in menu"
+            :key="index"
+            :to="item.link"
+          >
+            <div class="nuxt__link ma-2">
+              {{ item.label }}
+            </div>
+          </nuxt-link>
+
+          <div style="border-left: solid 1.5px black; height: 45px">
+            <v-menu class="ml-5" open-on-hover top offset-y>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn text color="black" v-bind="attrs" v-on="on" icon>
+                  <span style="margin-top: -1px; margin-left: 1rem">
+                    ID <v-icon>mdi-chevron-down</v-icon>
+                  </span>
+                </v-btn>
+              </template>
+
+              <v-list>
+                <v-list-item v-for="(item, index) in lang" :key="index">
+                  <v-list-item-title class="lang">{{
+                    item.title
+                  }}</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </div>
+        </div>
+      </v-container>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="font-weight-bold">
+                H I G L O B
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider></v-divider>
+
+          <v-list-item>
+            <v-list-item-title>Beranda</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-expansion-panels flat dense>
+              <v-expansion-panel>
+                <v-expansion-panel-header class="pa-0">
+                  <span style="font-size: 13px; font-weight: 500">Layanan</span>
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <v-list-item>
+                    <v-list-item-title>Persiapan Bahasa</v-list-item-title>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-list-item-title
+                      >Pendidikan Luar Negeri</v-list-item-title
+                    >
+                  </v-list-item>
+                  <v-list-item>
+                    <v-list-item-title>Working Holiday</v-list-item-title>
+                  </v-list-item>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Gallery</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title>Berita</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title>Tentang Kami</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title>Kontak</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-expansion-panels flat dense>
+              <v-expansion-panel>
+                <v-expansion-panel-header class="pa-0">
+                  <span style="font-size: 13px; font-weight: 500">Bahasa</span>
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <v-list-item>
+                    <v-list-item-title>Indonesia</v-list-item-title>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-list-item-title>Jepang</v-list-item-title>
+                  </v-list-item>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      lang: [
+        {
+          title: 'ID',
+        },
+        {
+          title: 'JP',
+        },
+      ],
+      group: null,
+      drawer: false,
+    }
+  },
+  mounted() {},
+  computed: {
+    menu() {
+      return [
+        {
+          label: 'Beranda',
+          link: '/',
+        },
+        {
+          label: 'Layanan',
+          link: '#syarat',
+        },
+        {
+          label: 'Berita',
+          link: '#langkah',
+        },
+        {
+          label: 'Galeri',
+          link: '/galeri',
+        },
+        {
+          label: 'Tentang Kami',
+          link: '#syarat',
+        },
+        {
+          label: 'Kontak',
+          link: '/kontak',
+        },
+      ]
+    },
+  },
+}
+</script>
+
+<style>
+</style>
